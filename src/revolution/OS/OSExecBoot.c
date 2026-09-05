ea8ae167a6a2f3bdb9536e369da08c1acef7f890
Quinn McHie <mchie.quinn@gmail.com>
2026-09-04 23:56:29 -0700

    os: correct OSBootDol ABI documentation

--- src/revolution/OS/OSExecBoot.c
+++ src/revolution/OS/OSExecBoot.c
@@ -1,6 +1,6 @@
 #include <revolution/OS.h>
 
-extern void __OSBootDol(u32 bootAddr, u32 dolAddr, u32* rebootInfo);
+extern void __OSBootDol(u32 doloffset, u32 restartCode, const char** argv);
 
 static u32 lbl_8055DA74;
 static u32 lbl_8055DA70;
@@ -13,7 +13,7 @@ void fn_80052390(u32 dolAddr, u32 bootAddr) {
     OSSetArenaLo((void*)0x81280000);
     OSSetArenaHi((void*)0x812F0000);
     dummy = 0;
-    __OSBootDol(bootAddr, dolAddr | 0x80000000, &dummy);
+    __OSBootDol(bootAddr, dolAddr | 0x80000000, (const char**)&dummy);
 }
 
 /* fn_800523F0 - read reboot params */
