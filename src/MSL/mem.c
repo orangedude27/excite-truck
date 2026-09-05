c58031c7ee90e20c3d7af1674589717d5ae9797a
Quinn McHie <mchie.quinn@gmail.com>
2026-09-04 23:14:33 -0700

    decomp: recover MSL memcmp

--- src/MSL/mem.c
+++ src/MSL/mem.c
@@ -14,6 +14,19 @@ void* memchr(const void* source, int value, size_t length) {
     return NULL;
 }
 
+int memcmp(const void* lhs, const void* rhs, size_t count) {
+    const unsigned char* p1;
+    const unsigned char* p2;
+
+    for (p1 = (const unsigned char*)lhs - 1, p2 = (const unsigned char*)rhs - 1, count++; --count;) {
+        if (*++p1 != *++p2) {
+            return (*p1 < *p2) ? -1 : 1;
+        }
+    }
+
+    return 0;
+}
+
 void* __memrchr(const void* source, int value, size_t length) {
     const unsigned char* current;
     unsigned long byte = value & 0xFF;
