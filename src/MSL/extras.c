180cde40a7f387e24b2286ae6a5b9c9801b7b77b
Quinn McHie <mchie.quinn@gmail.com>
2026-09-04 23:29:33 -0700

    decomp: port MSL string comparison helpers

---
+++ src/MSL/extras.c
@@ -0,0 +1,25 @@
+#include "extras.h"
+#include <cctype>
+
+int stricmp(const char* str1, const char* str2) {
+	char a_var;
+	char b_var;
+
+	do {
+		b_var = tolower(*str1++);
+		a_var = tolower(*str2++);
+
+		if (b_var < a_var) {
+			return -1;
+		}
+		if (b_var > a_var) {
+			return 1;
+		}
+	} while (b_var != 0);
+
+	return 0;
+}
+
+int strnicmp(const char* str1, const char* str2, int n) {
+    return __msl_strnicmp(str1, str2, n);
+}
